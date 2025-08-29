c9541ea96eac68ebd8985c8178d386f416bb6c3c
pipixia848 <HGY_taxue@163.com>
2025-08-29 15:06:00 +0800

    Add files via upload

---
+++ demo05/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+  import {ref, reactive} from 'vue'
+
+
+
+      // 定义一些要展示到html上的一些数据 变量/对象
+      let counter = ref(1);
+
+      // 绑定按键
+      function counterIncr() {
+        counter.value++;
+      }
+
+      function counterDecr() {
+        counter.value--;
+      }
+
+
+
+
+</script>
+
+<template>
+  <div>
+    <button @click="counterIncr()">+</button>
+    <span v-text="counter"></span>
+    <button @click="counterDecr()">-</button>
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
